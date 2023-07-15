7603b77841e016cd9f0b6ec16fb0ff7b76dc1649
Daniel Pineda <angeldpb27@gmail.com>
2023-07-14 20:10:07 -0500

    [+] ejercicio 5

---
+++ ejercicio5.js
@@ -0,0 +1,14 @@
+// 5.	Escribe una función que reciba un número como parámetro y determine si es un número primo. Devuelve true si es primo y false si no lo es. 
+
+function numeroPrimo(numero) {
+  for (let i = 2; i < numero; i++) {
+    if (numero % i === 0) {
+    return false;
+  }
+  else {
+    return true
+  }
+
+}}
+
+console.log(numeroPrimo(22));
